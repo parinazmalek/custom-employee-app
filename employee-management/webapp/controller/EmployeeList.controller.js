f2ff0b1805506f05856e9beca81dbc655a61a1f2
parinazmalek <pmalektabrizi@deloitte.ca>
2022-02-04 16:19:27 +0000

    Display employee list

--- employee-management/webapp/controller/EmployeeList.controller.js
+++ employee-management/webapp/controller/EmployeeList.controller.js
@@ -1,15 +1,40 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	'sap/ui/model/Sorter',
+	'sap/m/MessageBox',
+], function (Controller, Filter, FilterOperator, Sorter, MessageBox) {
+	"use strict";
 
         return Controller.extend("ca.deloitte.hr.employeemanagement.controller.EmployeeList", {
             onInit: function () {
-
+                this.oView = this.getView();
+                this._bDescendingSort = false;
+                this.oEmoloyeeTable = this.oView.byId("employeeTable");
+            },
+    
+            onSearch: function (oEvent) {
+                var oTableSearchState = [],
+                    sQuery = oEvent.getParameter("query");
+    
+                if (sQuery && sQuery.length > 0) {
+                    oTableSearchState = [new Filter("FirstName", FilterOperator.Contains, sQuery)];
+                }
+    
+                this.oEmoloyeeTable.getBinding("items").filter(oTableSearchState, "Application");
+            },
+    
+            onAdd: function () {
+                MessageBox.information("This functionality is not ready yet.", {title: "Aw, Snap!"});
+            },
+    
+            onSort: function () {
+                this._bDescendingSort = !this._bDescendingSort;
+                var oBinding = this.oEmoloyeeTable.getBinding("items"),
+                    oSorter = new Sorter("FirstName", this._bDescendingSort);
+    
+                oBinding.sort(oSorter);
             }
         });
     });
