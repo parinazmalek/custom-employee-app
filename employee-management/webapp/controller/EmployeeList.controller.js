be4f800a37baa6b56e327c3a7cce07154aa1f4dc
parinazmalek <pmalektabrizi@deloitte.ca>
2022-02-23 17:36:34 +0000

    filter and snapped text added

--- employee-management/webapp/controller/EmployeeList.controller.js
+++ employee-management/webapp/controller/EmployeeList.controller.js
@@ -1,43 +1,69 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator",
-	'sap/ui/model/Sorter',
-	'sap/m/MessageBox',
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    'sap/ui/model/Sorter',
+    'sap/m/MessageBox',
     "../model/formatter"
-], function (Controller, Filter, FilterOperator, Sorter, MessageBox,formatter) {
-	"use strict";
+], function (Controller, Filter, Sorter, MessageBox, formatter) {
+    "use strict";
 
-        return Controller.extend("ca.deloitte.hr.employeemanagement.controller.EmployeeList", {
-            formatter: formatter,
+    return Controller.extend("ca.deloitte.hr.employeemanagement.controller.EmployeeList", {
+        formatter: formatter,
 
-            onInit: function () {
-                this.oView = this.getView();
-                this._bDescendingSort = false;
-                this.oEmoloyeeTable = this.oView.byId("employeeTable");
-            },
-    
-            onSearch: function (oEvent) {
-                var oTableSearchState = [],
-                    sQuery = oEvent.getParameter("query");
-    
-                if (sQuery && sQuery.length > 0) {
-                    oTableSearchState = [new Filter("FirstName", FilterOperator.Contains, sQuery)];
-                }
-    
-                this.oEmoloyeeTable.getBinding("items").filter(oTableSearchState, "Application");
-            },
-    
-            onAdd: function () {
-                MessageBox.information("This functionality is not ready yet.", {title: "Aw, Snap!"});
-            },
-    
-            onSort: function () {
-                this._bDescendingSort = !this._bDescendingSort;
-                var oBinding = this.oEmoloyeeTable.getBinding("items"),
-                    oSorter = new Sorter("FirstName", this._bDescendingSort);
-    
-                oBinding.sort(oSorter);
+        onInit: function () {
+            this.oView = this.getView();
+            this._bDescendingSort = false;
+            this.oEmoloyeeTable = this.oView.byId("employeeTable");
+        },
+
+        onSearch: function (oEvent) {
+            var aFilter = [];
+            var aFilterSnappedText = [];
+            var oView = this.getView();
+
+            var filterName = oView.byId("filter_name").getValue();
+            if (filterName) {
+                aFilter.push(new Filter({
+                    path: "FirstName",
+                    operator: "Contains",
+                    value1: filterName
+                }));
+                aFilterSnappedText.push("name");
             }
-        });
+
+            var filterNumber = oView.byId("filter_employeeNumber").getValue();
+            if (filterNumber) {
+                aFilter.push(new Filter({
+                    path: "EmployeeNumber",
+                    operator: "Contains",
+                    value1: filterNumber
+                }));
+                aFilterSnappedText.push("number");
+
+            }
+
+            this.oEmoloyeeTable.getBinding("items").filter(aFilter, "Application");
+            this.getView().getModel("employeeModel").setProperty("/filterSnappedText",aFilterSnappedText.join(", "))
+        },
+
+        onAdd: function () {
+            MessageBox.information("This functionality is not ready yet.", { title: "Aw, Snap!" });
+        },
+
+        onSort: function () {
+            this._bDescendingSort = !this._bDescendingSort;
+            var oBinding = this.oEmoloyeeTable.getBinding("items"),
+                oSorter = new Sorter("FirstName", this._bDescendingSort);
+
+            oBinding.sort(oSorter);
+        },
+
+
+        onClear: function (oEvent) {
+            oEvent.getParameters().selectionSet.forEach(control => {
+                control.setValue("");
+            });
+            this.oEmoloyeeTable.getBinding("items").filter([], "Application");
+        }
     });
+});
